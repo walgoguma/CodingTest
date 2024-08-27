40007360b8826062e3ae228c11bc6c11eee4642b
walgoguma <90331768+walgoguma@users.noreply.github.com>
2024-08-27 22:56:23 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,26 @@
+N = int(input())
+setList = list(map(int, input().split()))
+
+maxNum = 1
+subSetList = [setList[0]]
+
+if len(setList)>1:
+    for idx, x in enumerate(setList[1:],1):
+        if x > subSetList[-1]:
+            subSetList.append(x)
+        else:
+            start = 0
+            end = len(subSetList)-1
+    
+            while start<end:
+                mid = (start + end) // 2
+                if subSetList[mid] >= x:
+                    end = mid
+                else:
+                    start = mid + 1
+    
+            subSetList[end] = x
+    
+        maxNum =  max(maxNum, len(subSetList))
+
+print(maxNum)
